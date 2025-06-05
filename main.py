e3518f477a891d51818429dbf667aa4b5dbd1a3c
dominicusadinfinitum <Dominik_Emmert@protonmail.com>
2025-06-05 17:27:14 +0200

    Draw a Player (Triangle)

--- main.py
+++ main.py
@@ -2,7 +2,8 @@
 # the open-source pygame library
 # throughout this file
 import pygame
-from constants import *  
+from constants import *
+from player import Player  
 
 
 
@@ -15,16 +16,19 @@ def main():
 
     clock = pygame.time.Clock()
     dt = 0
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60)/1000
         
-    
+        
+        
     
 
 if __name__ == "__main__":
